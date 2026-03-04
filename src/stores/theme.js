11d60cfd690fe354acb4ec3c778a50e33da1fc90
Ajaps <ajapsfranklin@gmail.com>
2026-03-04 02:39:51 +0100

    store

--- src/stores/theme.js
+++ src/stores/theme.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import { ref, watch } from 'vue'
 
 export const useThemeStore = defineStore('theme', () => {
-  const theme = ref(localStorage.getItem('theme') || 'dark')
+  const theme = ref(localStorage.getItem('theme') || 'light')
 
   function applyTheme(t) {
     document.documentElement.setAttribute('data-theme', t)
